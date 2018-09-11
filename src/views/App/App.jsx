f4dc2fa2916e346261666d3687c3a5e248640d3f
Harith Javed Bakhrani <harithjaved@gmail.com>
2018-09-11 12:51:39 +0300

    chore(bugsnag): setup error reporting tool (#88)
    - setup bugsnag to monitor the application and send error reports if any
    
    [Delivers #160157497]

--- src/views/App/App.jsx
+++ src/views/App/App.jsx
@@ -1,4 +1,6 @@
-import React, { Component } from 'react';
+import bugsnag from 'bugsnag-js'
+import React, { Component, Fragment } from 'react';
+import createPlugin from 'bugsnag-react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import moment from 'moment';
@@ -8,6 +10,16 @@ import Routes from '../../routes/index';
 import '../../../node_modules/material-design-lite/material';
 import '../../../node_modules/material-design-icons';
 import '../../../node_modules/toastr/build/toastr.min.css';
+
+
+// Check if the bugsnag API key was configured correctly
+// and handle cases where the key was not provided.
+let Wrapper = Fragment;
+
+if (process.env.REACT_APP_BUGSNAG_API_KEY) {
+  const bugsnagClient = bugsnag(process.env.REACT_APP_BUGSNAG_API_KEY)
+  let Wrapper = bugsnagClient.use(createPlugin(React))
+}
 
 // Calender settings
 moment.updateLocale('en', {
@@ -19,13 +31,15 @@ moment.updateLocale('en', {
 class App extends Component {
   render() {
     return (
-      <div className="App-container">
-        <Provider store={store}>
-          <BrowserRouter>
-            <Routes />
-          </BrowserRouter>
-        </Provider>
-      </div>
+      <Wrapper>
+        <div className="App-container">
+          <Provider store={store}>
+            <BrowserRouter>
+              <Routes />
+            </BrowserRouter>
+          </Provider>
+        </div>
+      </Wrapper>
     );
   }
 }
