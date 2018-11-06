e852e6e9bf3ee4e8079a6222e54e788cb19b0355
danielmaina <mainadaniel81@gmail.com>
2018-11-06 07:25:49 +0300

    chore(commitplease): configure commitplease to check commit messages are per playbook's standards (#269)
    - configure the commitplease package to check on commit messages pushed to the repo
    
    [Delivers #160751499]

--- src/redux/middleware/occupationSaga.js
+++ src/redux/middleware/occupationSaga.js
@@ -4,7 +4,7 @@ import { fetchOccupationsSuccess, fetchOccupationsFailure } from '../actionCreat
 import OccupationAPI from '../../services/OccupationAPI';
 import apiErrorHandler from '../../services/apiErrorHandler';
 
-// get occupations from API and dispatch action to get this data to store via reducer
+// get occupations from API and dispatch action to get this data to store through reducer
 export function* fetchOccupationsSaga(){
   try{
     const service = OccupationAPI.getOccupationData;
