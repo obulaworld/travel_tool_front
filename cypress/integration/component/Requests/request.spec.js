2811973012a02d6c03c2485392b7542f0f1d42a0
amarachukwu-agbo <amarachukwu.agbo@andela.com>
2018-11-29 16:45:45 +0100

    chore(end-to-end): user can create different request types
    - add tests for the request form
    - add tests for creating one-way, return and multi request
    
    [Delivers #161876890]

--- cypress/integration/component/Requests/request.spec.js
+++ cypress/integration/component/Requests/request.spec.js
@@ -7,8 +7,7 @@ describe('Requests', () => {
 
   describe('New User\'s request page', () => {
     before(() => {
-      const token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySW5mbyI6eyJpZCI6Ii1MSEptS3J4QThTbFBOUUZPVlZtIiwiZmlyc3RfbmFtZSI6IkFtYXJhY2h1a3d1IiwibGFzdF9uYW1lIjoiQWdibyIsImZpcnN0TmFtZSI6IkFtYXJhY2h1a3d1IiwibGFzdE5hbWUiOiJBZ2JvIiwiZW1haWwiOiJhbWFyYWNodWt3dS5hZ2JvQGFuZGVsYS5jb20iLCJuYW1lIjoiQW1hcmFjaHVrd3UgQWdibyIsInBpY3R1cmUiOiJodHRwczovL2xoNC5nb29nbGV1c2VyY29udGVudC5jb20vLUo4eVFYZHR2TkJJL0FBQUFBQUFBQUFJL0FBQUFBQUFBQUFBL0FHRGd3LWktZXZvMElaZ0JHYVFBZWgyY25OSUZRRUlKYncvbW8vcGhvdG8uanBnP3N6PTUwIiwicm9sZXMiOnsiVGVjaG5vbG9neSI6Ii1LWEg3aU1FNGViTUVYQUVjN0hQIiwiQW5kZWxhbiI6Ii1LaWloZlpvc2VRZXFDNmJXVGF1In19LCJpYXQiOjE1NDI5MDE3MTksImV4cCI6MTU0NTQ5MzcxOSwiYXVkIjoiYW5kZWxhLmNvbSIsImlzcyI6ImFjY291bnRzLmFuZGVsYS5jb20ifQ.Hfm3FGyu0pZRbRniRsvUZXOMVEogPE4t8R2vnOkrJfifsleYRa1NXaoZ1cTwwrkUpLCCclpCAkHCWsy2p05FVsbOiVZmHLlH88Er2xJHxinlCOhWWAHYkWgh8pPTt3csplC8hS2h4ZyVBz7ghvizNbyHiRP_XkPx_aAUHnQ7ICA';
-      cy.authenticateUser(token);
+      cy.authenticateUser();
       cy.visit('/requests');
     });
 
