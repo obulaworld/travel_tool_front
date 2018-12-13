15ed4c14da84b8e2a24da103d34194870beb2966
Michael Nthiwa <mike.nthiwa@gmail.com>
2018-12-13 14:28:27 +0300

    chore(session-expire): test user is logged out when token expires (#398)
    - tests that user is logged out when token expires.
    
    [Delivers #162368846]

---
+++ cypress/integration/component/session/logout.spec.js
@@ -0,0 +1,14 @@
+describe('Logout a user when token expires', () => {
+  it('should set token in cookies',  () =>  {
+    cy.authenticateUser();
+    cy.visit('/requests');
+  });
+
+  it('should check the url', () => {
+    cy.clearCookie('jwt-token');
+    cy.visit('/requests');
+    cy.get('.toast-message').should('be.visible')
+      .contains('Session Expired. Login to continue');
+    cy.url().should('eq', 'http://localhost:3000/');
+  });
+});
