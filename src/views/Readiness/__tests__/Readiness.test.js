3ed91f2978fb54534747653bafd63f5827fcae6e
Tawio sunday <taiwo.sunday@outlook.com>
2019-01-10 23:28:43 +0100

    feat(travelReadiness): a user can view details of a travel document - The user can see a list of travel document they have uplaoded - The user can click on the document a see a modal with the details of the document
    [devlived #163135271]

--- src/views/Readiness/__tests__/Readiness.test.js
+++ src/views/Readiness/__tests__/Readiness.test.js
@@ -4,20 +4,42 @@ import { MemoryRouter } from 'react-router-dom';
 import configureStore from 'redux-mock-store';
 import { Provider } from 'react-redux';
 import ConnectedReadiness from '../index';
+import { initialState } from '../../../redux/reducers/travelReadinessDocuments';
+import users from '../../TravelReadinessDocuments/__mocks__/users';
 
 const props = {
   closeModal: sinon.spy(() => Promise.resolve()),
   openModal: sinon.spy(() => Promise.resolve()),
   modalType: 'add visa',
+  userReadiness:  sinon.spy(() => Promise.resolve()),
+
   shouldOpen: true,
   createTravelReadinessDocument: sinon.spy(() => Promise.resolve()),
+  user: {
+    currentUser:{
+      userId: '1200'
+    }
+  },
+  currentUser: {
+    userId: '1200'
+  }
 };
 
 const state = {
   createTravelReadinessDocument: {},
+  travelReadinessDocuments: {
+    ...initialState,
+    users,
+    userReadiness: users[0],
+  },
   modal: {
     modal: {
 
+    }
+  },
+  user: {
+    currentUser:{
+      userId: '1200'
     }
   }
 };
--- src/views/Readiness/index.jsx
+++ src/views/Readiness/index.jsx
@@ -1,56 +1,171 @@
-import React, {Component, Fragment} from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
-import  PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import ReadinessPanelHeader from '../../components/ReadinessPanelHeader/ReadinessPanelHeader';
 import AddVisaForm from '../../components/Forms/TravelReadinessForm/AddVisaForm';
-import {closeModal, openModal} from '../../redux/actionCreator/modalActions';
+import { closeModal, openModal } from '../../redux/actionCreator/modalActions';
 import Modal from '../../components/modal/Modal';
-import {createTravelReadinessDocument} from '../../redux/actionCreator/travelReadinessActions';
+import Button from '../../components/buttons/Buttons';
+import { createTravelReadinessDocument } from '../../redux/actionCreator/travelReadinessActions';
+import { fetchUserReadinessDocuments } from '../../redux/actionCreator/travelReadinessDocumentsActions';
+import TravelReadinessDetailsTable from '../TravelReadinessDocuments/UserTravelReadinessDetails/UserTravelReadinessDetailsTable';
+import './Readiness.scss';
 
 export class TravelReadinessDocuments extends Component {
+  state = {
+    activeDocument: 'passport',
+    documentId: ''
+  };
+
+  componentDidMount() {
+    const { fetchUserData, user } = this.props;
+    fetchUserData(user.currentUser.userId);
+  }
+
+  toggleDocumentTab = type => {
+    this.setState({
+      activeDocument: type
+    });
+  };
+
+  showDocumentDetail = documentId => {
+    const { openModal } = this.props;
+    this.setState({
+      documentId
+    });
+    openModal(true, 'document details');
+  };
 
   renderVisaModal = () => {
-    const {closeModal, shouldOpen, modalType, createTravelReadinessDocument, travelReadinessDocuments} = this.props;
+    const {
+      closeModal,
+      shouldOpen,
+      modalType,
+      createTravelReadinessDocument,
+      travelReadinessDocuments
+    } = this.props;
     return (
       <Modal
-        customModalStyles="add-document-item" closeModal={closeModal} width="580px" height="600px"
+        customModalStyles="add-document-item"
+        closeModal={closeModal}
+        width="580px"
+        height="600px"
         visibility={
-          shouldOpen && (modalType === 'add visa') ? 'visible' : 'invisible'
+          shouldOpen && modalType === 'add visa' ? 'visible' : 'invisible'
         }
         title="Add Visa"
       >
-        <AddVisaForm closeModal={closeModal} createTravelReadinessDocument={createTravelReadinessDocument} {...travelReadinessDocuments} />
+        <AddVisaForm
+          closeModal={closeModal}
+          createTravelReadinessDocument={createTravelReadinessDocument}
+          {...travelReadinessDocuments}
+        />
       </Modal>
     );
   };
 
-  renderVisaPage = () => {
-    const { openModal } = this.props;
-    return (
-      <ReadinessPanelHeader openModal={openModal} />
-    );
-  };
-
-
-  render() {
+  renderPanelHeader() {
+    const { activeDocument } = this.state;
+    const { userReadiness, isLoading } = this.props;
+    const {
+      travelDocuments: { passport, visa }
+    } = userReadiness;
     return (
       <Fragment>
-        {this.renderVisaPage()}
+        {!isLoading && (
+          <div className="space-header">
+            <div className="open-requests">
+              <div className="button-group">
+                <Button
+                  showBadge
+                  badge={passport && passport.length}
+                  onClick={() => this.toggleDocumentTab('passport')}
+                  text="Passports"
+                  buttonClass={`bg-btn bg-btn--with-badge ${
+                    activeDocument === 'passport' ? 'bg-btn--active' : ''
+                  }`}
+                  badgeClass={
+                    activeDocument === 'passport'
+                      ? 'bg-btn--with-badge--active'
+                      : 'bg-btn--with-badge__approvals--inactive'
+                  }
+                />
+                <Button
+                  badge={visa && visa.length}
+                  showBadge
+                  buttonClass={`bg-btn bg-btn--with-badge ${
+                    activeDocument === 'visa' ? 'bg-btn--active' : ''
+                  }`}
+                  badgeClass={
+                    activeDocument === 'visa'
+                      ? 'bg-btn--with-badge--active'
+                      : 'bg-btn--with-badge__approvals--inactive'
+                  }
+                  text="Visas"
+                  onClick={() => this.toggleDocumentTab('visa')}
+                />
+              </div>
+            </div>
+          </div>
+        )}
+      </Fragment>
+    );
+  }
+
+  renderVisaPage = () => {
+    const { openModal } = this.props;
+    return <ReadinessPanelHeader openModal={openModal} />;
+  };
+
+  render() {
+    const { activeDocument, documentId } = this.state;
+    const {
+      userReadiness,
+      isLoading,
+      shouldOpen,
+      modalType,
+      closeModal
+    } = this.props;
+    const {
+      travelDocuments: { passport, visa }
+    } = userReadiness;
+    return (
+      <Fragment>
+        <div style={{ width: '96%' }}>{this.renderVisaPage()}</div>
         {this.renderVisaModal()}
+        <div className={isLoading ? 'readiness-header' : ''}>
+          {this.renderPanelHeader()}
+        </div>
+        <TravelReadinessDetailsTable
+          closeModal={closeModal}
+          shouldOpen={shouldOpen}
+          modalType={modalType}
+          isLoading={isLoading}
+          activeDocument={activeDocument}
+          passports={passport}
+          visas={visa}
+          handleShowDocument={this.showDocumentDetail}
+          documentId={documentId}
+          userData={userReadiness}
+        />
       </Fragment>
     );
   }
 }
 
-const mapStateToProps = ({modal, travelReadinessDocuments}) => ({
+const mapStateToProps = ({ modal, travelReadinessDocuments, user }) => ({
   ...modal.modal,
-  travelReadinessDocuments
+  travelReadinessDocuments,
+  userReadiness: travelReadinessDocuments.userReadiness,
+  isLoading: travelReadinessDocuments.isLoading,
+  user: user
 });
 
 const matchDispatchToProps = {
   openModal,
   closeModal,
-  createTravelReadinessDocument
+  createTravelReadinessDocument,
+  fetchUserData: fetchUserReadinessDocuments
 };
 
 TravelReadinessDocuments.propTypes = {
@@ -65,4 +180,7 @@ TravelReadinessDocuments.defaultProps = {
   modalType: 'add visa'
 };
 
-export default connect(mapStateToProps, matchDispatchToProps)(TravelReadinessDocuments);
+export default connect(
+  mapStateToProps,
+  matchDispatchToProps
+)(TravelReadinessDocuments);
