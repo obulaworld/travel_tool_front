8bf1d8fdb40d5a1be8e67870366830eb08f7996f
Mirriam <31400129+Mirriam-Maina@users.noreply.github.com>
2019-01-21 15:46:37 +0300

    chore(fix date picker user experience): fix user experience when using datepicker (#495)
    - add an input field for typing in the date
    
    [Finishes #163286419]

--- src/components/Forms/FormsAPI/Input/InputFields/Date.jsx
+++ src/components/Forms/FormsAPI/Input/InputFields/Date.jsx
@@ -5,40 +5,55 @@ import { PropTypes } from 'prop-types';
 import 'react-datepicker/dist/react-datepicker.css';
 import calendarIcon from '../../../../../images/icons/calendar_icon.svg';
 
-const DateInput = props => {
-  const { error, value, className, name} = props;
-  let selectedDate = value
-    ? moment(value, 'MM-DD-YYYY')
-    : null;
+class DateInput extends React.Component {
+  
+  state = {
+    selectedDate: null,
+  }
 
+  static getDerivedStateFromProps (nextProps, nextState) {
+    const { value } = nextProps;
+    return { selectedDate: value ? moment(nextProps.value, 'MM-DD-YYYY') : null};
+  }
 
-  return (
-    <div className={`date-wrapper ${className}`} id={`${name}_date`}>
-      <DatePicker
-        className={`${error ? 'error' : ''}`}
-        calendarClassName="calendar-body"
-        dayClassName={() => 'calendar-day'}
-        fixedHeight
-        selected={selectedDate}
-        {...props}
-      />
-      <img className="calendar-icon" src={calendarIcon} alt="cal" />
-    </div>
-  );
-};
+  handleChange=(date, event)=> {
+    const { onChange } = this.props;
+    this.setState({selectedDate: date });
+    date && onChange(date, event);
+  }
+
+  render(){
+    const { error, className, name, onBlur } = this.props;
+    const { selectedDate } = this.state;
+    return (
+      <div className={`date-wrapper ${className}`} id={`${name}_date`}>
+        <DatePicker
+          className={`${error ? 'error' : ''}`}
+          calendarClassName="calendar-body"
+          dayClassName={() => 'calendar-day'}
+          fixedHeight
+          placeholderText="MM/DD/YYYY"
+          selected={selectedDate}
+          onChange={(date, event) => this.handleChange(date, event)}
+          name={name}
+          onBlur={onBlur}
+        />
+        <img className="calendar-icon" src={calendarIcon} alt="cal" />
+      </div>
+    ); 
+  }
+}
+
 
 DateInput.propTypes = {
-  value: PropTypes.oneOfType([
-    PropTypes.object,
-    PropTypes.string
-  ]),
   error: PropTypes.string,
   className: PropTypes.string,
   name:  PropTypes.string,
+  onBlur: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired
 };
 
 DateInput.defaultProps = {
-  value: null,
   className: '',
   error: '',
   name: ''
