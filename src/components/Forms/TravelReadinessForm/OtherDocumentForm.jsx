0c9b256b30f6243bd80e0874aa9dbfdacd6ee3d2
Moses Gitau <gitaumoses4@gmai.com>
2019-01-22 22:33:30 +0300

    chore(travel-readiness): add progress bar for travel readiness documents
    - display progress when uploading a travel document
    - refactor the PassportForm to use the BaseForm
    
    [Finishes #163286529]

--- src/components/Forms/TravelReadinessForm/OtherDocumentForm.jsx
+++ src/components/Forms/TravelReadinessForm/OtherDocumentForm.jsx
@@ -1,76 +1,24 @@
-import React, { Component } from 'react';
-import {PropTypes} from 'prop-types';
-import BaseForm from './BaseForm';
+import React from 'react';
+import withTravelReadinessForm from '../../Hoc/withTravelReadinessForm';
+import OtherDocumentFieldSet from './FormFieldsets/OtherDocumentFieldSet';
 
-class OtherDocumentForm extends Component{
-  constructor(props) {
-    super(props);
-    this.defaultState = {
-      values: {
-        name: '',
-        dateOfIssue: '',
-        expiryDate: '',
-      },
-      image: '',
-      imageChanged: false,
-      documentUploaded: false,
-      uploadingDocument: false,
-      errors: {},
-      hasBlankFields: true,
-      optionalFields: [
-        'documentId',
-        'dateOfIssue'
-      ]
-    };
-    this.state = {...this.defaultState};
-  }
+export default withTravelReadinessForm(OtherDocumentFieldSet,'other',
+  {
+    values: {
+      name: '',
+      dateOfIssue: '',
+      expiryDate: '',
+    },
+    image: '',
+    imageChanged: false,
+    documentUploaded: false,
+    uploadingDocument: false,
+    errors: {},
+    hasBlankFields: true,
+    optionalFields: [
+      'documentId',
+      'dateOfIssue'
+    ]
+  });
 
-  render() {
-    const { 
-      fetchUserData, 
-      user,closeModal, 
-      createTravelReadinessDocument,
-      editTravelReadinessDocument,
-      travelReadinessDocuments,
-      document,
-      currentDocument,
-      modalType
-    } = this.props;
-    const { errors } = this.state;
-    return (
-      <BaseForm
-        fetchUserData={fetchUserData}
-        user={user}
-        errors={errors}
-        defaultFormState={this.state}
-        documentType="other"
-        {...travelReadinessDocuments}
-        closeModal={closeModal}
-        createTravelReadinessDocument={createTravelReadinessDocument}
-        editTravelReadinessDocument={editTravelReadinessDocument}
-        currentDocument={currentDocument}
-        modalType={modalType}
-        document={document}
-      />
-    );
-  }
-}
 
-OtherDocumentForm.propTypes = {
-  createTravelReadinessDocument: PropTypes.func.isRequired,
-  closeModal: PropTypes.func.isRequired,
-  fetchUserData: PropTypes.func.isRequired,
-  user: PropTypes.object.isRequired,
-  openModal: PropTypes.func.isRequired,
-  editTravelReadinessDocument: PropTypes.func,
-  document: PropTypes.object,
-  modalType: PropTypes.string,
-};
-
-OtherDocumentForm.defaultProps = {
-  editTravelReadinessDocument: () => {},
-  document: {},
-  modalType: ''
-};
-
-export default OtherDocumentForm;
