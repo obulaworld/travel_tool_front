f2038895c18197a5f1f211cfb683cc4f439bf312
johnkegz <johnkal24@gmail.com>
2019-01-24 14:51:18 +0300

    chore(e2e): add, edit, delete and view the details of their passports.
    - enable users create a passport
    - enable users add a comment on passport details modal
    - enable users edit the passport information
    - enable users delete a passport
    
    [Delivers #163421100]

---
+++ cypress/integration/component/passport/passport.spec.js
@@ -0,0 +1,211 @@
+const baseAPI = Cypress.env('REACT_APP_API_URL');
+const userName = Math.random().toString(36).substr(2).toUpperCase();
+describe('Add passport page ', () => {
+  before(() => {
+    cy.authenticateUser();
+    cy.visit('/travel_readiness');
+  });
+  describe('Add passport form', () => {
+    before(() => {
+      cy.get('button#passportButton')
+        .wait(2000)
+        .click();
+      cy.get('button.documents-button-group__button')
+        .click();
+    });
+    it(`should display in a modal when the
+        	'Add passport' button is clicked`, () => {
+      cy.get('.modal')
+        .as('passport-modal')
+        .should('be.visible');
+      cy.get('@passport-modal')
+        .find('form.travel-document-form')
+        .as('add-passport-form')
+        .should('be.visible');
+      cy.get('@add-passport-form').contains('Name as Seen On Passport');
+      cy.get('@add-passport-form').contains('Passport Number');
+      cy.get('@add-passport-form').contains('Nationality');
+      cy.get('@add-passport-form').contains('Place of Issue');
+      cy.get('@add-passport-form').contains('Expiry Date');
+      cy.get('@add-passport-form').contains('Attach File');
+    });
+
+    it(`should show a validation error if
+          user puts the cursor in an input field and leaves its without filling it`, () => {
+      cy.get('input[name=name]')
+        .focus()
+        .blur();
+      cy.get('input[name=name] + span').as('missing-name');
+      cy.get('@missing-name').should('have.class', 'error');
+      cy.get('@missing-name').contains('This field is required');
+      cy.get('input[name=passportNumber]')
+        .focus()
+        .blur();
+      cy.get('input[name=passportNumber] + span').as('missing-passportNumber');
+      cy.get('@missing-passportNumber').should('have.class', 'error');
+      cy.get('@missing-passportNumber').contains('This field is required');
+    });
+  });
+
+  describe('Add passport', () => {
+    it('should successfully add a passport', () => {
+      cy.server();
+      cy.route('POST', `${baseAPI}/travel_readiness`);
+      cy.get('input[name=name]')
+        .wait(2000)
+        .type(Math.random().toString(36).substr(2).toUpperCase());
+      cy.get('input[name=passportNumber]')
+        .wait(2000)
+        .type(Math.random().toString(36).substr(2).toUpperCase());
+      cy.get('input[name=nationality]')
+        .wait(2000)
+        .type('Ugandan');
+      cy.get('input[name=dateOfBirth]')
+        .wait(2000)
+        .type('01/01/1999');
+      cy.get('input[name=dateOfIssue]')
+        .wait(2000)
+        .type('01/01/2018');
+      cy.get(':nth-child(6) > input')
+        .wait(2000)
+        .type('Uganda');
+      cy.get('input[name=expiryDate]')
+        .wait(2000)
+        .type('{downarrow}{enter}');
+      cy.uploadFile('images/guesthouse.jpg', 'image/jpeg');
+      cy.get('button#submit')
+        .should('not.be.disabled')
+        .click();
+      cy.authenticateUser()
+        .wait(2000);
+      cy.get('.toast-message')
+        .should('be.visible')
+        .wait(3000)
+        .contains('Passport created successfully!');
+      cy.get('.modal-title-bar')
+        .should('be.visible');
+      cy.get('button#yes')
+        .should('be.visible')
+        .click();
+    });
+
+    it('should add second passport successfully', () => {
+      cy.get('input[name=name]')
+        .wait(2000)
+        .type(userName);
+      cy.get('input[name=passportNumber]')
+        .wait(2000)
+        .type(Math.random().toString(36).substr(2).toUpperCase());
+      cy.get('input[name=nationality]')
+        .wait(2000)
+        .type('Ugandan');
+      cy.get('input[name=dateOfBirth]')
+        .wait(2000)
+        .type('01/01/1999');
+      cy.get('input[name=dateOfIssue]')
+        .wait(2000)
+        .type('01/01/2018');
+      cy.get(':nth-child(6) > input')
+        .wait(2000)
+        .type('Uganda');
+      cy.get('input[name=expiryDate]')
+        .wait(2000)
+        .type('{downarrow}{enter}');
+      cy.uploadFile('images/guesthouse.jpg', 'image/jpeg');
+      cy.get('button#submit')
+        .should('not.be.disabled')
+        .click();
+      cy.authenticateUser()
+        .wait(2000);
+      cy.get('.toast-message')
+        .wait(3000)
+        .should('be.visible')
+        .contains('Passport created successfully!')
+        .wait(2000);
+      cy.get('.modal-title-bar')
+        .should('be.visible');
+      cy.get('button#no')
+        .should('be.visible')
+        .click();
+      cy.get('.rp-requests')
+        .should('be.visible');
+    });
+  });
+
+  describe('Edit passport', () => {
+    it('edit passport information on the system', ()=>{
+      cy.get('.table__body >:last-child > :nth-child(8) > .menu__container > :nth-child(1) > #toggleIcon2')
+        .click();
+      cy.get(`.table__body >:last-child > :nth-child(8) > .menu__container > :nth-child(1) > 
+      .table__menu-container > .table__menu-list`)
+        .contains('Edit');
+      cy.get(`.table__body >:last-child > :nth-child(8) > 
+      .menu__container > :nth-child(1) > .table__menu-container > .table__menu-list > #iconBtn2`)
+        .click();
+      cy.get('input[name=name]')
+        .wait(2000)
+        .should('have.value', userName)
+        .clear()
+        .type(`Requester ${Math.random().toString(36).substr(2).toUpperCase()}`);
+      cy.get('input[name=dateOfIssue]')
+        .clear()
+        .wait(2000)
+        .type('01/01/2016');
+      cy.get('#submit')
+        .click();
+      cy.get('.toast-message')
+        .wait(3000)
+        .should('be.visible')
+        .contains('Passport updated successfully!')
+        .wait(2000);
+    });
+  });
+
+  describe('Delete passport', () => {
+    it('user should be able to delete a passport', ()=> {
+      cy.get('#toggleIcon2')
+        .click();
+      cy.get('.table__menu-list')
+        .contains('Delete');
+      cy.get('#deleteRequest')
+        .click();
+      cy.get('.modal')
+        .as('delete-modal')
+        .should('be.visible');
+      cy.get('@delete-modal')
+        .find('button.bg-btn')
+        .contains('Delete')
+        .click().wait(3000);
+      cy.get('.toast-message')
+        .contains('Document successfully deleted')
+        .wait(2000);
+    });
+  });
+
+  describe('Comment on passport information', () => {
+    it('users should be able to comment on passport information', ()=> {
+      cy.get(':nth-child(2) > :nth-child(1) > .document-name')
+        .click();
+      cy.get('.modal')
+        .as('comment-modal')
+        .should('be.visible')
+        .contains('Passport Details');
+      cy.get('@comment-modal')
+        .find('.ql-editor')
+        .as('comment-editor')
+        .should('be.visible');
+      cy.get('@comment-editor')
+        .type('Hello, World');
+      cy.get('#post-submit')
+        .contains('Post')
+        .click()
+        .wait(2000);
+      cy.get('.modal__status-update > p')
+        .should('be.visible')
+        .contains('Hello,World');
+      cy.get('.modal-close > img')
+        .should('be.visible')
+        .click(); 
+    });
+  });
+});
