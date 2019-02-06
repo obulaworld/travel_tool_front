5569e9b71123378fb0d3a04c420c930140148cc5
phunmbi <phunmbi@gmail.com>
2019-02-06 17:21:03 +0100

    chore(countryFlags): add placeholder flag
    - add placeholder flag for countries not on country-data
    
    [Finishes #163759075]

--- src/helper/countryUtils.js
+++ src/helper/countryUtils.js
@@ -1,15 +1,16 @@
 import { lookup } from 'country-data';
+import placeholderFlag from './placeholderFlag/placeholderFlag.svg';
 
 class countryUtils {
   static getCountryCode(country) {
     const countryCode = lookup.countries({name: country})[0];
-    return countryCode.alpha2;
+    return countryCode;
   }
-  
+
   static getCountryFlagUrl(location) {
     const country = location.split(', ')[1];
     const countryCode = this.getCountryCode(country);
-    return `https://www.countryflags.io/${countryCode}/flat/64.png`;
+    return countryCode ? `https://www.countryflags.io/${countryCode.alpha2}/flat/64.png` : placeholderFlag;
   }
 }
 export default countryUtils;
