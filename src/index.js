c922fdc11ef14cbcc861a95127c6f7397a459602
Samuel Kubai <kamausamuel11@gmail.com>
2019-02-12 17:06:52 +0300

    feat(analytics): setup analytics in travela (#570)
    - add fullstory code snippet to the index file
    - add Google analytics code snippet in the initialization file
    
    [Delivers #163897324]

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
+
 import './index.scss';
 import App from './views/App';
 import registerServiceWorker from './registerServiceWorker';
+
+// Register Google analytics tracking for the application
+// and the various events for Google analytics to track.
+ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_KEY);
+ReactGA.pageview('/home');
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
