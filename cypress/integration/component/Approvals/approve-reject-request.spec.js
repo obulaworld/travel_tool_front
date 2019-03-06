3a40ce8c16c4f6425c740218424a61cb782d536d
Uchechukwu Akogwu <34472497+ucheg6@users.noreply.github.com>
2019-03-06 20:09:03 +0100

    #164292651 test that a manager can approve and reject a request (#640)
    chore(e2e): test that a manager can approve and reject a request
     - write e2e test to ensure a manager can approve and reject a request on the new request page
    
    [Finishes #164292651]

--- cypress/integration/component/Approvals/approve-reject-request.spec.js
+++ cypress/integration/component/Approvals/approve-reject-request.spec.js
@@ -1,36 +1,21 @@
 const baseAPI = Cypress.env('REACT_APP_API_URL');
+const userDataURL = /api\/v1\/user\/\W+/;
 
 describe('Approval page(Approval or rejection of request by managers)', () => {
   beforeEach(() => {
     cy.authenticateUser();
     cy.server();
-    cy.route('GET', `${baseAPI}/user/-LSsFyueC086niFc9rrz`).as('getUserDetails');
+    cy.route('GET', userDataURL).as('getUserDetails');
     cy.visit('/requests/my-approvals').wait(3000);
     cy.wait('@getUserDetails').wait(3000);
   });
-  it(`should open request modal and show approved badge when an approved
-          request is clicked`, () => {
-    cy.get('.request__status--approved:first').parent().parent().parent().parent().as('approvedRow');
-    cy.get('@approvedRow').find('div.button-outline').click();
-    cy.get('.modal__button-below > .request__status--approved')
-      .should('be.visible')
-      .contains('Approved');
-  });
-
-  it(`should open request modal and show rejected badge when a rejected
-  request is clicked`, () => {
-    cy.get('.request__status--rejected:first').parent().parent().parent().parent().as('rejectedRow');
-    cy.get('@rejectedRow').find('div.button-outline').click();
-    cy.get('.modal__button-below > .request__status--rejected')
-      .should('be.visible')
-      .contains('Rejected');
-  });
-
+  
   it('should approve a request and toast a successful approval message', () => {
     cy.get('.request__status--open:first').parent().parent().parent().parent().as('openRow');
     cy.get('@openRow').find('div.button-outline').click();
-    cy.get('#b1').click();
-    cy.get('#Approve').click();
+    cy.get('.action-button--approve').click();
+    cy.get('#approve').click();
+    cy.get('.action-button--disabled').contains('reject').should('be.disabled');
     cy.get('.toast-message')
       .wait(3000)
       .should('be.visible')
@@ -40,10 +25,29 @@ describe('Approval page(Approval or rejection of request by managers)', () => {
   it('should reject a request and toast a successful rejection message', () => {
     cy.get('.request__status--open:first').parent().parent().parent().parent().as('openRow');
     cy.get('@openRow').find('div.button-outline').click();
-    cy.get('#b2').click();      
-    cy.get('#Reject').click();
+    cy.get('.action-button--reject').click();     
+    cy.get('#reject').click();
+    cy.get('.action-button--disabled').contains('approve').should('be.disabled');
     cy.get('.toast-message')
       .should('be.visible')
       .contains('Request rejected successfully');
+  });
+
+  it(`should open request and show approved badge when an approved
+          request is clicked`, () => {
+    cy.get('.request__status--approved:first').parent().parent().parent().parent().as('approvedRow');
+    cy.get('@approvedRow').find('div.button-outline').click();
+    cy.get('.action-button--approved')
+      .should('be.visible')
+      .contains('approved');
+  });
+
+  it(`should open request and show rejected badge when a rejected
+  request is clicked`, () => {
+    cy.get('.request__status--rejected:first').parent().parent().parent().parent().as('rejectedRow');
+    cy.get('@rejectedRow').find('div.button-outline').click();
+    cy.get('.action-button--rejected')
+      .should('be.visible')
+      .contains('rejected');
   });
 });
